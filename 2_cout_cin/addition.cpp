bf1408c916d555196f28d0d780d1fb233898a9da
Masterchief-07 <kjonathan1999@hotmail.com>
2023-05-11 05:17:28 +0000

    started implementation

---
+++ 2_cout_cin/addition.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main()
+{
+    //project addidition
+    int a{0}, b{0}, result{0};
+    std::cout<<"welcome to addition software\n";
+    std::cout<<"Input a:";
+    std::cin>>a;
+    std::cout<<"Input b:";
+    std::cin>>b;
+    result = a + b;
+    std::cout<<"result: "<<result<<"\n";
+}
---
+++ Introduction/helloworld.cpp
@@ -0,0 +1,9 @@
+
+#include <iostream>
+
+int main()
+{
+    std::cout<<"Hello, worlds!"<<std::endl;
+
+    return 0;
+}
